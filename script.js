cfee3df779fade79142002f700883eedb913aef6
Sumitsaman <94039583+Sumitsaman@users.noreply.github.com>
2023-08-31 16:32:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+
+// Function to handle registration button click
+function handleRegisterClick() {
+    alert('Thank you for registering for the webinar!');
+  }
+  
+  // Add event listener to the registration button
+  const registerButton = document.querySelector('.cta-button');
+  registerButton.addEventListener('click', handleRegisterClick);
+  
